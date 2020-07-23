2bd8f0ef339f6be75305098aa1590bc17cd966cd
Marcin <podhajskimarcin@gmail.com>
2020-07-23 18:15:08 +0200

    add k_l_minimality

--- k_l_minimality.cpp
+++ k_l_minimality.cpp
@@ -22,7 +22,7 @@ private:
     static bool isSubset(vector<int> A, vector<int> B) {
         std::sort(A.begin(), A.end());
         std::sort(B.begin(), B.end());
-        return std::includes(A.begin(), A.end(), B.begin(), B.end());
+        return std::includes(B.begin(), B.end(), A.begin(), A.end());
     }
 
     static std::vector<std::vector<int>> subsets(const std::vector<int> &input, int l) {
@@ -55,11 +55,122 @@ private:
         TemporalConstraintLanguage temporalConstraintLanguage(relations);
         std::vector<TemporalConstraint> constraints = cspInstance.constraints;
         for (auto subset: subsets(variables, size)) {
+            bool shouldAdd = true;
+            for (auto constraint: cspInstance.constraints) {
+                if (isSubset(subset, constraint.variables)) {
+                    shouldAdd = false;
+                    break;
+                }
+            }
+            if (!shouldAdd) {
+                continue;
+            }
             TemporalConstraint constraint(temporalConstraintLanguage, relations.size() - 1, subset);
             constraints.push_back(constraint);
         }
         TemporalCSPInstance temporalCspInstance(temporalConstraintLanguage, constraints);
         return temporalCspInstance;
+    }
+
+    static vector<int> normalize(vector<int> list, std::vector<int> subset, std::vector<int> variables) {
+        std::vector<int> result(subset.size());
+        for (int j = 0; j < subset.size(); j++) {
+            for (int i = 0; i < variables.size(); i++) {
+                if (subset[j] == variables[i]) {
+                    result[j] = list[i];
+                }
+            }
+        }
+        std::set<int> sortedTuple(result.begin(), result.end());
+        int i = 0;
+        std::unordered_map<int, int> normalizedTuple;
+        for (auto variable: sortedTuple) {
+            normalizedTuple[variable] = i;
+            i++;
+        }
+        list.resize(result.size());
+        for (i = 0; i < result.size(); i++) {
+            int newValue = normalizedTuple[result[i]];
+            list[i] = newValue;
+        }
+        return list;
+    }
+
+    static bool remove_not_in_intersection(TemporalCSPInstance &cspInstance, int constraint1Index, int constraint2Index,
+                                           std::vector<int> subset) {
+        std::unordered_map<int, std::vector<int>> linearOrderSubset1;
+        std::unordered_map<int, std::vector<int>> linearOrderSubset2;
+        auto order1 = cspInstance.constraintLanguage.relations[cspInstance.constraints[constraint1Index].relation].tuples;
+        vector<WeakLinearOrder> constraint1(order1.begin(), order1.end());
+        auto order2 = cspInstance.constraintLanguage.relations[cspInstance.constraints[constraint2Index].relation].tuples;
+        vector<WeakLinearOrder> constraint2(order2.begin(), order2.end());
+        for (int i = 0; i < constraint1.size(); i++) {
+            auto linearOrder = constraint1[i];
+            auto list = linearOrder.toList();
+            list = normalize(list, subset, cspInstance.constraints[constraint1Index].variables);
+            linearOrderSubset1[i] = list;
+        }
+        for (int i = 0; i < constraint2.size(); i++) {
+            auto linearOrder = constraint2[i];
+            auto list = linearOrder.toList();
+            list = normalize(list, subset, cspInstance.constraints[constraint2Index].variables);
+            linearOrderSubset2[i] = list;
+        }
+        std::vector<int> remainingOrders1, remainingOrders2;
+        for (int i = 0; i < constraint1.size(); i++) {
+            for (int j = 0; j < constraint2.size(); j++) {
+                if(linearOrderSubset1[i] == linearOrderSubset2[j]){
+                    remainingOrders1.push_back(i);
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < constraint2.size(); i++) {
+            for (int j = 0; j < constraint1.size(); j++) {
+                if(linearOrderSubset2[i] == linearOrderSubset1[j]){
+                    remainingOrders2.push_back(i);
+                    break;
+                }
+            }
+        }
+        if(remainingOrders1.size() == constraint1.size() && remainingOrders2.size() == constraint2.size()){
+            return false;
+        }
+        if(remainingOrders1.size() != constraint1.size()){
+            std::set<WeakLinearOrder> weakLinearOrders;
+            for(auto i: remainingOrders1){
+                weakLinearOrders.insert(constraint1[i]);
+            }
+            TemporalRelation relation(constraint1[0].list.size(), weakLinearOrders);
+            std::vector<TemporalRelation> relations = cspInstance.constraintLanguage.relations;
+            relations.push_back(relation);
+            TemporalConstraintLanguage language(relations);
+            std::vector<TemporalConstraint> constraints = cspInstance.constraints;
+            for(auto &constraint: constraints){
+                constraint.constraintLanguage = language;
+            }
+            constraints[constraint1Index].relation = relations.size() - 1;
+            TemporalCSPInstance instance(language, constraints);
+            cspInstance = instance;
+        }
+        if(remainingOrders2.size() != constraint2.size()){
+            std::set<WeakLinearOrder> weakLinearOrders;
+            for(auto i: remainingOrders2){
+                weakLinearOrders.insert(constraint2[i]);
+            }
+            TemporalRelation relation(constraint2[0].list.size(), weakLinearOrders);
+            std::vector<TemporalRelation> relations = cspInstance.constraintLanguage.relations;
+            relations.push_back(relation);
+            TemporalConstraintLanguage language(relations);
+            std::vector<TemporalConstraint> constraints = cspInstance.constraints;
+            for(auto &constraint: constraints){
+                constraint.constraintLanguage = language;
+            }
+            constraints[constraint2Index].relation = relations.size() - 1;
+            TemporalCSPInstance instance(language, constraints);
+            cspInstance = instance;
+        }
+        return true;
     }
 
     static TemporalCSPInstance remove(TemporalCSPInstance cspInstance, int size) {
@@ -70,15 +181,14 @@ private:
             for (int i = 1; i <= size; i++) {
                 for (auto subset: subsets(variables, i)) {
                     for (int j = 0; j < cspInstance.constraints.size(); j++) {
-                        if(!isSubset(subset, cspInstance.constraints[j].variables)){
+                        if (!isSubset(subset, cspInstance.constraints[j].variables)) {
                             continue;
                         }
                         for (int k = 0; k < cspInstance.constraints.size(); k++) {
-                            if (k == j || !isSubset(subset, cspInstance.constraints[k].variables)){
+                            if (k == j || !isSubset(subset, cspInstance.constraints[k].variables)) {
                                 continue;
                             }
-
-                            if (false) {
+                            if (remove_not_in_intersection(cspInstance, j, k, subset)) {
                                 endFlag = false;
                             }
                         }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -70,21 +70,18 @@ public:
     }
 
     static void test_kl_minimality() {
-        WeakLinearOrder order1(3, {2, 1, 0});
-        WeakLinearOrder order2(3, {2, 0, 1});
-        WeakLinearOrder order3(3, {1, 2, 0});
-        WeakLinearOrder order4(3, {1, 0, 2});
-        WeakLinearOrder order5(3, {1, 0, 0});
-        WeakLinearOrder order6(3, {1, 0, 1});
-        WeakLinearOrder order7(3, {1, 1, 0});
+        WeakLinearOrder order1(3, {1, 0, 2});
+        WeakLinearOrder order2(3, {0, 0, 1});
+        WeakLinearOrder order3(3, {0, 1, 2});
+        WeakLinearOrder order4(3, {0, 0, 1});
+        TemporalRelation relation(3, {order1, order2});
+        TemporalRelation relation2(3, {order3, order4});
 
-        TemporalRelation relation(3, {order1, order2, order3, order4, order5, order6,
-                                      order7}); // R = {(a,b,c) : a>b v a>c} //0=2,1=1,3=0,2=0
-        TemporalConstraintLanguage temporalConstraintLanguage({relation});
+        TemporalConstraintLanguage temporalConstraintLanguage({relation, relation2});
         TemporalConstraint constraint1(temporalConstraintLanguage, 0, {0, 1, 2});
-        TemporalConstraint constraint2(temporalConstraintLanguage, 0, {1, 0, 3});
+        TemporalConstraint constraint2(temporalConstraintLanguage, 1, {0, 1, 2});
         TemporalCSPInstance temporalCspInstance(temporalConstraintLanguage,
-                                                {constraint1, constraint2}); //R(x_0,x_1,x_2)^R(x_1,x_0,x_3)
+                                                {constraint1, constraint2});
         TemporalCSPInstance klMinimalInstance = KLMinimality::solve(temporalCspInstance, 2, 3);
         assert(Backtracking::solve(klMinimalInstance));
     }
