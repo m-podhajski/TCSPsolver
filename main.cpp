a93f65323391c0da218ffc9df77d95c8a6270823
Marcin <podhajskimarcin@gmail.com>
2020-08-08 17:38:07 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
+#include <iostream>
 #include "tests/tests.cpp"
+
 int main() {
-    Tests::test_ll_closed();
-    Tests::test_mx_closed();
-    Tests::test_mi_closed();
-    Tests::test_min_closed();
-    Tests::test_backtracking();
-    Tests::test_kl_minimality();
+    WeakLinearOrder order1(4, {0, 0, 1, 1}); // {(a,b,c,d) : (a=b<c=d)}
+    WeakLinearOrder order2(4, {0, 1, 2, 3}); // {(a,b,c,d) : (a<b<c<d)}
+
+    TemporalRelation relation1(4, {order1, order2}); // R_0 = {(a,b,c,d) : (a=b^b<c^c=d)v(a<b<c<d)}
+    TemporalConstraintLanguage temporalConstraintLanguage({relation1}); //sklada sie z jednej relacji - R_0
+
+
+    TemporalConstraint constraint1(temporalConstraintLanguage, 0,
+                                   {1, 2, 3, 4}); // R_0(x_1,x_2,x_3,x_4) - relacja R_0 o indeksie 0
+    TemporalConstraint constraint2(temporalConstraintLanguage, 0,
+                                   {1, 2, 4, 5}); // R_0(x_1,x_2,x_4,x_5) - relacja R_0 o indeksie 0
+    TemporalConstraint constraint3(temporalConstraintLanguage, 0,
+                                   {1, 2, 5, 3}); // R_0(x_1,x_2,x_5,x_3) - relacja R_0 o indeksie 0
+    TemporalCSPInstance temporalCspInstance(temporalConstraintLanguage,
+                                            {constraint1, constraint2,
+                                             constraint3}); // R(x_1,x_2,x_3,x_4)^R(x_1,x_2,x_4,x_5)^R(x_1,x_2,x_5,x_3)
+
+    std::cout << LLClosed::solve(temporalCspInstance) << std::endl;
     return 0;
 }
